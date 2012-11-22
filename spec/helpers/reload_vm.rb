676d93993669badbf261b36a4be6b5f2d360e0c1
Peter Zotov <whitequark@whitequark.org>
2012-11-22 21:47:52 +0400

    Fix specs.

--- spec/helpers/reload_vm.rb
+++ spec/helpers/reload_vm.rb
@@ -1,6 +1,9 @@
 def reload_vm!
   Foundry.send :remove_const, :VI
+  Foundry::Interpreter.send :remove_const, :VI
   load File.expand_path('../../../lib/foundry/vm.rb', __FILE__)
+
+  Foundry::Interpreter.const_set :VI, Foundry::VI
 
   $f = Foundry::Runtime.new
   $f.bootstrap
