e30a174134f671708333bca14bf40a73d9ba3ca6
Peter Zotov <whitequark@whitequark.org>
2012-12-07 17:55:49 +0400

    Const-related metamethods.

--- vm/common/module.rb
+++ vm/common/module.rb
@@ -33,6 +33,34 @@ class Module
   def included(mod)
   end
 
+  def const_get(name, search_parent=true)
+    if @constant_table.key? name
+      @constant_table[name]
+    elsif search_parent && @superclass
+      @superclass.const_get(name, search_parent)
+    else
+      raise NameError, "uninitialized constant #{name} for #{self}"
+    end
+  end
+
+  def const_set(name, value)
+    if @constant_table.key? name
+      raise NameError, "already initialized constant #{name} for #{self}"
+    else
+      @constant_table[name] = value
+    end
+  end
+
+  def constants(search_parent=true)
+    list = @constant_table.keys
+
+    if search_parent && @superclass
+      list |= @superclass.constants
+    end
+
+    list
+  end
+
   def public(*)
     # TODO
   end
