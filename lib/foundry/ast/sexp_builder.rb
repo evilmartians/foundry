942018d389c8ee761c438a326ae7753d11fd12fa
Peter Zotov <whitequark@whitequark.org>
2012-12-21 19:14:50 +0400

    Add s_const and s_send to SexpBuilder.

--- lib/foundry/ast/sexp_builder.rb
+++ lib/foundry/ast/sexp_builder.rb
@@ -3,5 +3,25 @@ module Foundry
     def s(type, *children)
       AST::Node.new(type, children)
     end
+
+    def s_const(name)
+      name.split('::').reduce(nil) do |ast, name_part|
+        if name_part.empty?
+          s(:const_base)
+        elsif ast.nil?
+          s(:const_ref, s(:var, :Cref), name_part.to_sym)
+        else
+          s(:const_fetch, ast, name_part.to_sym)
+        end
+      end
+    end
+
+    def s_send(receiver, method, *args)
+      s(:send,
+        receiver,
+        s(:symbol, method),
+        s(:tuple, *args),
+        s(:nil))
+    end
   end
 end
---
+++ spec/ast/sexp_builder_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe 'AST::SexpBuilder' do
+  include Foundry::AST::SexpBuilder
+
+  it 'builds non-nested consts' do
+    s_const('Foundry').should ==
+      s(:const_ref, s(:var, :Cref), :Foundry)
+  end
+
+  it 'builds nested consts' do
+    s_const('A::B::C').should ==
+      s(:const_fetch,
+        s(:const_fetch,
+          s(:const_ref, s(:var, :Cref), :A),
+          :B),
+        :C)
+  end
+
+  it 'builds toplevel consts' do
+    s_const('::A').should ==
+      s(:const_fetch,
+        s(:const_base),
+        :A)
+  end
+
+  it 'builds calls' do
+    s_call(s(:foo), :bar, s(:integer, 1), s(:string, "2")).should ==
+      s(:send,
+        s(:foo),
+        s(:symbol, :bar),
+        s(:tuple, s(:integer, 1), s(:string, "2")),
+        s(:nil))
+  end
+end
