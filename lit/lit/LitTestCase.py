5afec685d2660e0db1bd31077e112b55965ed961
Peter Zotov <whitequark@whitequark.org>
2013-08-15 13:10:39 +0400

    Import lit from LLVM.

---
+++ lit/lit/LitTestCase.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+import unittest
+
+import lit.Test
+
+"""
+TestCase adaptor for providing a 'unittest' compatible interface to 'lit' tests.
+"""
+
+class UnresolvedError(RuntimeError):
+    pass
+        
+class LitTestCase(unittest.TestCase):
+    def __init__(self, test, lit_config):
+        unittest.TestCase.__init__(self)
+        self._test = test
+        self._lit_config = lit_config
+
+    def id(self):
+        return self._test.getFullName()
+
+    def shortDescription(self):
+        return self._test.getFullName()
+
+    def runTest(self):
+        tr, output = self._test.config.test_format.execute(
+            self._test, self._lit_config)
+
+        if tr is lit.Test.UNRESOLVED:
+            raise UnresolvedError(output)
+        elif tr.isFailure:
+            self.fail(output)
