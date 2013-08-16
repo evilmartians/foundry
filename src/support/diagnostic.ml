e39a123483a672cd1c51dc34a90a0e1d1addbc0b
Peter Zotov <whitequark@whitequark.org>
2013-08-16 10:51:03 +0400

    Diagnostic.print: don't print escape sequences to non-ttys.

--- src/support/diagnostic.ml
+++ src/support/diagnostic.ml
@@ -23,18 +23,22 @@ let string_of_severity severity =
   | Note    -> "note"
 
 let print diag =
+  (* Don't print escape sequences to non-terminals. *)
+  let isatty  = Unix.isatty (Unix.stdin) in
+  let color x = if isatty then x else "" in
+
   let severity, message, locations = diag in
   let loc = (List.hd locations) in
   let file, (line_beg, col_beg), (line_end, col_end) = Location.decompose loc in
     prerr_endline (
-        "\x1b[1m" ^ file ^ ":" ^
+        (color "\x1b[1m") ^ file ^ ":" ^
         (string_of_int line_beg) ^ ":" ^
         (string_of_int (col_beg + 1)) ^ ": " ^
-        (color_of_severity severity) ^
-        (string_of_severity severity) ^ ":\x1b[1;37m " ^
-        message ^ "\x1b[0m");
+        (color (color_of_severity severity)) ^
+        (string_of_severity severity) ^ ":" ^ (color "\x1b[1;37m") ^ " " ^
+        message ^ (color "\x1b[0m"));
     prerr_endline (Location.line_source loc);
     if line_beg = line_end then
       prerr_endline (
-        (String.make col_beg ' ') ^ "\x1b[1;32m" ^
-        (String.make (col_end - col_beg) (Char.of_string "^")) ^ "\x1b[0m")
+        (String.make col_beg ' ') ^ (color "\x1b[1;32m") ^
+        (String.make (col_end - col_beg) (Char.of_string "^")) ^ (color "\x1b[0m"))
