48f0da96caf48586cb767c35dd5940db7ecfd9ef
Peter Zotov <whitequark@whitequark.org>
2013-08-22 17:11:51 +0400

    Add datalayout in foundry_gen.

--- src/tools/foundry_gen.ml
+++ src/tools/foundry_gen.ml
@@ -30,6 +30,14 @@ let _ =
 
   let llmod = Codegen.llvm_module_of_ssa_capsule capsule in
 
+  (* cortex-m3 *)
+  Llvm.set_target_triple "thumbv7m-none--eabi" llmod;
+  Llvm.set_data_layout   "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:64:128-a0:0:64-n32-S64" llmod;
+
+  (* x86_64 *)
+  Llvm.set_target_triple "x86_64-pc-linux-gnu" llmod;
+  Llvm.set_data_layout   "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80:128:128-n8:16:32:64-S128" llmod;
+
   if !dump then
     Llvm.dump_module llmod
   else if !output = "" then
