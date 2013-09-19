8f5776364bc813f8f484d609cbeaa32ee27f70e7
Peter Zotov <whitequark@whitequark.org>
2013-09-19 15:38:25 +0400

    myocamlbuild: -w @12 (Redundant sub-pattern in a pattern-matching.)

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -24,7 +24,7 @@ dispatch begin function
 
     flag ["ocaml"; "compile"; "bin_annot"] (A"-bin-annot");
 
-    flag ["ocaml"; "compile"] (S[A"-w"; A"@5@8@10@11@14@23@24@26@29"]);
+    flag ["ocaml"; "compile"] (S[A"-w"; A"@5@8@10@11@12@14@23@24@26@29"]);
 
     if do_cov then
       tag_any ["package(bisect)"]
