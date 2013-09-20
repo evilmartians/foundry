5df839fa17ad1c95b12b8122c3df36848fbe71ad
Peter Zotov <whitequark@whitequark.org>
2013-09-20 17:32:09 +0400

    Specialization: log more information.

--- src/transforms/specialization.ml
+++ src/transforms/specialization.ml
@@ -4,53 +4,53 @@ open Ssa
 let name = "Specialization"
 
 let run_on_function passmgr capsule funcn =
-  let overload orig_funcn ty =
-    let orig_func  = func_of_name orig_funcn in
-    let best_funcn =
-      (* Try to find a more specific function, but not too specific. Unification
-         of such function with the signature must produce the signature itself. *)
-      try
-        Ssa.find_overload capsule (Option.default orig_funcn orig_func.f_original)
-          ~f:(fun overload_ty overload ->
-            try
-              let env = Typing.meaningful (Typing.unify overload_ty ty) in
-              if Rt.equal ty (Typing.subst env ty) then begin
-                (* Amount of the substitutions is used as a measurement of specifity. *)
-                if Rt.equal overload_ty ty then
-                  Some 99999 (* Exactly our type. *)
-                else
-                  Some (List.length env)
-              end else
-                None
-            with Typing.Conflict _ ->
-              None)
-      with Not_found ->
-        orig_funcn
-    in
-    (* Check if we need to specialize this function even more, or it's
-       specific enough. *)
-    let env = Typing.meaningful (Typing.unify best_funcn.ty ty) in
-    if Rt.equal (Typing.subst env best_funcn.ty) best_funcn.ty then
-      (* Unification produced a signature exactly equal to the overload
-         being considered. *)
-      best_funcn
-    else begin
-      (* Unification changed the signature of callee. *)
-      let funcn' = copy_func best_funcn in
-      add_func     capsule funcn';
-      add_overload capsule (Option.default orig_funcn orig_func.f_original) ty funcn';
-      ignore (specialize funcn' env);
-
-      (* Visit newly added function. *)
-      Pass_manager.mark ~reason:((Rt.inspect_type orig_funcn.ty) ^ " => " ^
-                                 (Rt.inspect_type funcn'.ty))
-                        passmgr funcn';
-
-      funcn'
-    end
-  in
-
   iter_uses funcn ~f:(fun instr ->
+    let position = "@" ^ (block_parent (instr_parent instr)).id ^ ":%" ^ instr.id in
+    let overload orig_funcn ty =
+      let orig_func  = func_of_name orig_funcn in
+      let best_funcn =
+        (* Try to find a more specific function, but not too specific. Unification
+           of such function with the signature must produce the signature itself. *)
+        try
+          Ssa.find_overload capsule (Option.default orig_funcn orig_func.f_original)
+            ~f:(fun overload_ty overload ->
+              try
+                let env = Typing.meaningful (Typing.unify overload_ty ty) in
+                if Rt.equal ty (Typing.subst env ty) then begin
+                  (* Amount of the substitutions is used as a measurement of specifity. *)
+                  if Rt.equal overload_ty ty then
+                    Some 99999 (* Exactly our type. *)
+                  else
+                    Some (List.length env)
+                end else
+                  None
+              with Typing.Conflict _ ->
+                None)
+        with Not_found ->
+          orig_funcn
+      in
+      (* Check if we need to specialize this function even more, or it's
+         specific enough. *)
+      let env = Typing.meaningful (Typing.unify best_funcn.ty ty) in
+      if Rt.equal (Typing.subst env best_funcn.ty) best_funcn.ty then
+        (* Unification produced a signature exactly equal to the overload
+           being considered. *)
+        best_funcn
+      else begin
+        (* Unification changed the signature of callee. *)
+        let funcn' = copy_func best_funcn in
+        add_func     capsule funcn';
+        add_overload capsule (Option.default orig_funcn orig_func.f_original) ty funcn';
+        ignore (specialize funcn' env);
+
+        (* Visit newly added function. *)
+        Pass_manager.mark ~reason:((Rt.inspect_type orig_funcn.ty) ^ " => " ^
+                                   (Rt.inspect_type funcn'.ty) ^ " at " ^ position)
+                          passmgr funcn';
+
+        funcn'
+      end
+    in
     (* If the call site signature and callee types do not match,
        unify them and replace the callee with a specialized
        function. *)
@@ -63,7 +63,7 @@ let run_on_function passmgr capsule funcn =
           (* Visit the caller again: specialization may have opened new
              opportunities. *)
           let caller = block_parent (instr_parent instr) in
-          Pass_manager.mark ~reason:("specialized %" ^ instr.id)
+          Pass_manager.mark ~reason:("specialized at " ^ position)
                             passmgr caller
         end;
         callee'
