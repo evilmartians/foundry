68ca78ed409527e153c3f1714c9d2b4afc376e81
Peter Zotov <whitequark@whitequark.org>
2013-09-21 19:39:01 +0400

    scripts: use ulimit to prevent runaway tests.

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,7 +8,8 @@ fi
 
 echo "Testing foundry..."
 OCAMLRUNPARAM=Rb ./test_foundry.native; echo
-./vendor/lit/lit.py -v -s --no-progress-bar test/
+(ulimit -t 5 ; ulimit -d 512000 ; ulimit -m 512000 ; ulimit -s 8192
+ ./vendor/lit/lit.py -v -s --no-progress-bar test/)
 
 if [ -n "$COVERAGE" ]; then
   echo "Generating coverage..."
