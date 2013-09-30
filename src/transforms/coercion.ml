38e361730069b662cc8f84fb0a6f7b7224c8f408
Peter Zotov <whitequark@whitequark.org>
2013-09-30 15:17:18 +0400

    Coercion: preliminary signed coercion.

--- src/transforms/coercion.ml
+++ src/transforms/coercion.ml
@@ -11,12 +11,19 @@ let run_on_function passmgr capsule funcn =
       when prim = "int_coerce"
     -> (let value =
           match instr.ty with
-          | Rt.UnsignedTy(width)
+          | Rt.UnsignedTy (width)
           -> (let coerced = mod_big_int value (shift_left_big_int unit_big_int width) in
               if coerced <> value then
                 failwith "too narrow"
               else
                 Some (const (Rt.Unsigned (width, coerced))))
+          | Rt.SignedTy (width)
+          -> (assert ((sign_big_int value) <> -1);
+              let coerced = mod_big_int value (shift_left_big_int unit_big_int width) in
+              if coerced <> value then
+                failwith "too narrow"
+              else
+                Some (const (Rt.Signed (width, coerced))))
           | _
           -> None
         in
