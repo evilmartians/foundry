baaaff37c767e52b8c24df51175509e96a122885
Peter Zotov <whitequark@whitequark.org>
2013-10-01 13:15:08 +0400

    scripts: make test.sh examine LIT_ARGS.

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,10 +6,15 @@ if [ -n "$COVERAGE" ]; then
   find test -name 'bisect*.out' -delete
 fi
 
+if [ -z "$LIT_ARGS" ]; then
+  LIT_ARGS="-v -s --no-progress-bar"
+fi
+
 echo "Testing foundry..."
 OCAMLRUNPARAM=Rb ./test_foundry.native; echo
+
 (ulimit -t 5 ; ulimit -d 512000 ; ulimit -m 512000 ; ulimit -s 8192
- ./vendor/lit/lit.py -v -s --no-progress-bar test/)
+ ./vendor/lit/lit.py $LIT_ARGS test/)
 
 if [ -n "$COVERAGE" ]; then
   echo "Generating coverage..."
